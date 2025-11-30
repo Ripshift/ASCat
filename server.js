22c55c8f06ea233f13cc490eb8a3a61e0011187c
Rippen <97502323+Ripshift@users.noreply.github.com>
2025-11-30 02:05:48 -0800

    server update

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const { serveHTTP, getRouter } = require('stremio-addon-sdk');
+const { serveHTTP } = require('stremio-addon-sdk');
 const addonInterface = require('./addon');
-const express = require('express');
-const path = require('path');
 
 const PORT = process.env.PORT || 7000;
 
-serveHTTP(addonInterface, { port: PORT }).then(({ server, app }) => {
-    // Serve static files from public directory
-    app.use(express.static(path.join(__dirname, 'public')));
-    
-    console.log(`Stremio addon is running on http://localhost:${PORT}`);
-    console.log(`Install in Stremio with: http://localhost:${PORT}/manifest.json`);
-}).catch((error) => {
-    console.error('Error starting addon:', error);
-    process.exit(1);
-});
+serveHTTP(addonInterface, { port: PORT })
+    .then(() => {
+        console.log(`Stremio addon is running on http://localhost:${PORT}`);
+        console.log(`Install in Stremio with: http://localhost:${PORT}/manifest.json`);
+    })
+    .catch((error) => {
+        console.error('Error starting addon:', error);
+        process.exit(1);
+    });
+
+module.exports = addonInterface;
